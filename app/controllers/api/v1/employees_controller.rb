0bf66efa0fc2a76d6ee92ade40fb6bd6c7388cf7
Mittal Patel <mittal8991@gmail.com>
2026-02-27 18:42:59 +0530

    added employee show endpoint along with serializer adjustment

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -13,6 +13,11 @@ class Api::V1::EmployeesController < ApplicationController
     render json: employees, status: :ok
   end
 
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee, status: :ok
+  end
+
   private
 
   def employee_params
--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,3 +1,7 @@
 class EmployeeSerializer < ActiveModel::Serializer
   attributes :id, :full_name, :job_title, :country, :salary
+
+  def salary
+    object.salary.to_f
+  end
 end
