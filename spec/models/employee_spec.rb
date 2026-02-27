c48ccf6e7743bb080ea151dd38b76fb31345be92
Mittal Patel <mittal8991@gmail.com>
2026-02-27 13:03:26 +0530

    added failing specs for employee validations

---
+++ spec/models/employee_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Employee do
+  it { is_expected.to validate_presence_of(:full_name) }
+  it { is_expected.to validate_presence_of(:job_title) }
+  it { is_expected.to validate_presence_of(:country) }
+  it { is_expected.to validate_presence_of(:salary) }
+end
