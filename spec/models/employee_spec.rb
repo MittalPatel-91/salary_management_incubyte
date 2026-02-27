f974c27337e1fe41db92839f7456a7adeda76f6a
Mittal Patel <mittal8991@gmail.com>
2026-02-27 15:38:49 +0530

    refactor employee model spec

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Employee do
-  it { is_expected.to validate_presence_of(:full_name) }
-  it { is_expected.to validate_presence_of(:job_title) }
-  it { is_expected.to validate_presence_of(:country) }
-  it { is_expected.to validate_presence_of(:salary) }
+RSpec.describe Employee, type: :model do
+  subject { build(:employee) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:full_name) }
+    it { is_expected.to validate_presence_of(:job_title) }
+    it { is_expected.to validate_presence_of(:country) }
+    it { is_expected.to validate_presence_of(:salary) }
+  end
 end
