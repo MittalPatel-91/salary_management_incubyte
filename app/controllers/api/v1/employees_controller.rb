725424a5cdab2b0e2217bf80ae2b0fa5424ce857
Mittal Patel <mittal8991@gmail.com>
2026-02-27 16:15:06 +0530

    added employee controller with create endpoint

---
+++ app/controllers/api/v1/employees_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::EmployeesController < ApplicationController
+  def create
+    employee = Employee.create!(employee_params)
+    render json: employee, status: :created
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:full_name, :job_title, :country, :salary)
+  end
+end
