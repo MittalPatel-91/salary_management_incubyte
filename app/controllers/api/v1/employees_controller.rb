f07d4c9ceb530c64a26c95f27256685495317280
Mittal Patel <mittal8991@gmail.com>
2026-03-03 11:41:29 +0530

    implemented employee update endpoint

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::EmployeesController < ApplicationController
+  before_action :set_employee, only: [ :update ]
+
   def create
     employee = Employee.new(employee_params)
     if employee.save
@@ -22,9 +24,25 @@ class Api::V1::EmployeesController < ApplicationController
     end
   end
 
+  def update
+    if @employee
+      if @employee.update(employee_params)
+        render json: @employee, status: :ok
+      else
+        render json: { errors: @employee.errors.full_messages }, status: :unprocessable_entity
+      end
+    else
+      render json: { error: "Employee not found" }, status: :not_found
+    end
+  end
+
   private
 
   def employee_params
     params.require(:employee).permit(:full_name, :job_title, :country, :salary)
+  end
+
+  def set_employee
+    @employee = Employee.find_by(id: params[:id])
   end
 end
